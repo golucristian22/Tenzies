bab73586841a67a635323b2b1b335d3705255c2a
Cristian <golu.cristian22@gmail.com>
2022-07-08 19:36:17 +0300

    Made the Game Design and Applyed the Styles.

--- src/components/tenzies/tenzies.component.jsx
+++ src/components/tenzies/tenzies.component.jsx
@@ -12,8 +12,17 @@ const Tenzies = () => {
           </p>
         </header>
         <div className="tenzies__content">
-          <div className="tenzies__number-container">
+          <div className="tenzies__numbers-container">
+            <div className="tenzies__number tenzies__number--correct">2</div>
+            <div className="tenzies__number">1</div>
             <div className="tenzies__number">2</div>
+            <div className="tenzies__number">3</div>
+            <div className="tenzies__number">4</div>
+            <div className="tenzies__number">5</div>
+            <div className="tenzies__number">6</div>
+            <div className="tenzies__number">7</div>
+            <div className="tenzies__number">8</div>
+            <div className="tenzies__number">9</div>
           </div>
           <button className="tenzies__button">Roll</button>
         </div>
