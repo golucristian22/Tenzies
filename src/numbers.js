fcfb6e5e085c550eb5e01e9a848140f2601e0c91
Cristian <golu.cristian22@gmail.com>
2022-07-09 13:38:52 +0300

    Fixed the Missing id That Caused a Bug.

--- src/numbers.js
+++ src/numbers.js
@@ -2,42 +2,52 @@ const numbersObj = [
   {
     id: 1,
     number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
   {
     id: 2,
     number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
   {
     id: 3,
     number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
   {
     id: 4,
     number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
   {
     id: 5,
     number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
   {
     id: 6,
     number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
+  },
+  {
+    id: 7,
+    number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
   {
     id: 8,
     number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
   {
     id: 9,
     number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
   {
     id: 10,
     number: Math.floor(Math.random() * 6 + 1),
-  },
-  {
-    id: 11,
-    number: Math.floor(Math.random() * 6 + 1),
+    hold: false,
   },
 ];
 
