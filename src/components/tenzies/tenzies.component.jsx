b018f97022e6fb86755906534ae05c4df52a708e
Cristian <golu.cristian22@gmail.com>
2022-07-09 06:59:35 +0300

    Added Numbers Objesct & Each Number is Random.

--- src/components/tenzies/tenzies.component.jsx
+++ src/components/tenzies/tenzies.component.jsx
@@ -1,6 +1,16 @@
 import "./tenzies.styles.scss";
+import numbersObj from "../../numbers";
 
 const Tenzies = () => {
+  console.log(numbersObj);
+  const numbers = numbersObj.map((number, id) => {
+    return (
+      <div key={id} className="tenzies__number">
+        {number.number}
+      </div>
+    );
+  });
+
   return (
     <div className="tenzies__container">
       <div className="tenzies">
@@ -12,18 +22,7 @@ const Tenzies = () => {
           </p>
         </header>
         <div className="tenzies__content">
-          <div className="tenzies__numbers-container">
-            <div className="tenzies__number tenzies__number--correct">2</div>
-            <div className="tenzies__number">1</div>
-            <div className="tenzies__number">2</div>
-            <div className="tenzies__number">3</div>
-            <div className="tenzies__number">4</div>
-            <div className="tenzies__number">5</div>
-            <div className="tenzies__number">6</div>
-            <div className="tenzies__number">7</div>
-            <div className="tenzies__number">8</div>
-            <div className="tenzies__number">9</div>
-          </div>
+          <div className="tenzies__numbers-container">{numbers}</div>
           <button className="tenzies__button">Roll</button>
         </div>
       </div>
---
+++ src/numbers.js
@@ -0,0 +1,44 @@
+const numbersObj = [
+  {
+    id: 1,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 2,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 3,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 4,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 5,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 6,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 8,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 9,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 10,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+  {
+    id: 11,
+    number: Math.floor(Math.random() * 6 + 1),
+  },
+];
+
+export default numbersObj;
